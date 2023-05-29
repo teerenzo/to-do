6ab74915d76bf2f788b86331ec4877c5adbbd06b
teerenzo <renzahoemmanuel8@gmail.com>
2023-05-29 18:55:14 +0200

    init

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,12 @@
+type TodoItemProps={
+    id:string,
+    title:string,
+    complete:boolean
+}
+
+export default function TodoItem({id,title,complete}:TodoItemProps){
+return <li className="flex gap-1 items-center">
+    <input id={id} type="checkbox" className="cursor-pointer peer" />
+    <label htmlFor={id} className='peer-checked:line-through'>{title}</label>
+</li>
+}
